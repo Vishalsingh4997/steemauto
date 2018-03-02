f0032daad92a3a3edae112d34da5d6c4ecd0e598
Vishalsingh4997 <Vishalsingh4997@gmail.com>
2018-03-02 19:37:57 +0530

    Update claimreward.php
    Grammatical mistakes corrected!

--- inc/claimreward.php
+++ inc/claimreward.php
@@ -18,7 +18,7 @@ if($x == 1){
 				<h3>Welcome <? echo $name; ?>,</h3><br>
 				Here you can enable or disable SteemAuto from automatically claiming your rewards.<br><br>
 				<strong>More info:</strong> Usually you redeem your Steemit curation and author rewards by clicking on a button in your wallet on steemit.com<br>
-				This handy tool means you don't need to click on that button manually, since it does that job for you automatically!<br>
+				This handy tool means you don't need to click on that button manually since it does that job for you automatically!<br>
 				Every five minutes this tool will check your account and will transfer your pending rewards to your balance.<br>
 				This tool is safe to use because SteemAuto only has access to your posting authority. That means it can only claim your rewards and cannot access any of the funds in your wallet.<br><br>
 				<strong>Status:</strong> 
